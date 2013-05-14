56fabd341302a5c94e23d4f11f8de520d9fcfefa
Frank Lam <franklam@oursky.com>
2013-05-14 20:39:07 +0800

    First build for startlab.hk

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+$(window).scroll(function(){
+//box one
+    var $win = $(window);
+    $('#side-nav').css('top', $win.scrollTop());
+});
 
+$(document).ready(function() {
+
+    $("#menu-btn").on("click", function(e){
+            $("#mobile-menu").slideToggle();
+            $(this).toggleClass("active");
+            e.preventDefault();
+         });
+
+});
